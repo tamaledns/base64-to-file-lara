4719cbd32ee83b38754042b0cbefc487d1508339
Dennis <tamaledns@gmail.com>
2024-05-20 10:38:24 +0300

    first commit

---
+++ src/Base64ToUploadedFileProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace tamaledns\Base64ToFile;
+
+use Illuminate\Support\ServiceProvider;
+
+class Base64ToFileProvider extends ServiceProvider {
+    
+    public function boot () {
+
+    }
+
+    public function register() {
+        
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tamaledns/base64-to-uploaded-file',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'adbe7c4c41a5e100f54e5b280305cd2f4e5337d3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tamaledns/base64-to-uploaded-file' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'adbe7c4c41a5e100f54e5b280305cd2f4e5337d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
